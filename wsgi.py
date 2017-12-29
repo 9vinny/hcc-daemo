6ff5fe96e4e80e106c89901ee5cb1a6e12920bc5
비린덜 바하티아 <GARB42@Bir-Mac.local>
2017-12-29 14:26:08 +0900

    requirements and setting updated

--- wsgi.py
+++ wsgi.py
@@ -12,7 +12,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hcc-daemo.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "csp.settings")
 
 application = get_wsgi_application()
 _webserver = Cling(application)
